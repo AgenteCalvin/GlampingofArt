daece047c434019f8c9651b92c5361abe1c5d4dc
Glampingofart <roldanjuan77@gmail.com>
2024-11-05 23:15:40 -0500

    feature_cambio

--- app/src/main/java/com/example/glampingofart/loginactivity.kt
+++ app/src/main/java/com/example/glampingofart/loginactivity.kt
@@ -23,7 +23,7 @@ class loginactivity : AppCompatActivity() {
         setContentView(R.layout.loginactivity)
 
         /*
-        Extraer datos de las cajas de texto hola
+        Extraer datos de las cajas de texto hola x2
 
         box_usuario guarda el valor ingresado en usuario
         box_contraseña guarda el valor ingresado en contraseña
